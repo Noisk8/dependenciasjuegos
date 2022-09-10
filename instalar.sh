6a31540916f4ffe425dd02d4b8939b88085d1734
Noisk8 <juansk8jaramillo@gmail.com>
2022-09-10 18:41:58 -0500

    Create instalar.sh

---
+++ instalar.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+
+sudo apt install libsdl1.2debian
+sudo apt install libsdl1.2debian:i386
+
+sudo apt install libsdl2-image-2.0-0 
+sudo apt install libsdl2-image-2.0-0:i386
+
+sudo apt install libsdl2-2.0-0
+sudo apt install libsdl2-2.0-0:i386
+
+sudo apt install libcurl3
+sudo apt install libcurl3:i386
+
+sudo apt install libglu1
+sudo apt install libglu1:i386
+
+sudo apt install libfreetype6
+sudo apt install libfreetype6:i386
+
+sudo apt install libudev1
+sudo apt install libudev1:i386
+
+sudo apt-get install -y libcairo2
+sudo apt-get install -y libcairo2:i386
+
+sudo apt install libvorbisfile3:i386
+sudo apt install libvorbisfile3
+
+sudo apt install libcggl -y
+sudo apt install libcggl:i386
+
+sudo apt install libcanberra-gtk-module:i386
+sudo apt install libcanberra-gtk-module
+
+sudo apt-get install libfontconfig1:i386
+
+sudo apt install xboxdrv 
+
